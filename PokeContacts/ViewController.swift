6b4c7cf8ba6755d58eb2a1483dfe5cf7795d653b
jihyeon <dkswlgus0314@naver.com>
2024-07-16 12:07:20 +0900

    Initial Commit

---
+++ PokeContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeContacts
+//
+//  Created by ahnzihyeon on 7/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
